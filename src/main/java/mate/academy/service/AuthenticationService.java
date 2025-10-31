83787b450e7d2e6b5531d2245c1dbb541deddba9
Oleksandr <alexandrheletey@gmail.com>
2025-10-31 10:27:53 +0200

    fixed checkstyle

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -16,7 +16,7 @@ public class AuthenticationService {
     public boolean login(String email, String password) {
         User user = UserService.findByEmail(email);
 
-            return user != null && user.getPassword().equals(password);
+        return user != null && user.getPassword().equals(password);
     }
 }
 
